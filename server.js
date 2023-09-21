50901224e487ef1707996a9e544e7fc43bc95845
Hemanth kumar <rajva5727@gmail.com>
2023-09-21 23:16:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,25 +20,23 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-	res.send(`Running ${PORT}`);
+	res.send('Running');
 });
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
-
+	console.log(socket.id)
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
 	});
 
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		console.log("calling user",userToCall,"  ",from)
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
 	});
 
 	socket.on("answerCall", (data) => {
-		console.log("answered", data.signal)
 		io.to(data.to).emit("callAccepted", data.signal)
 	});
 });
 
-server.listen(PORT);
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
